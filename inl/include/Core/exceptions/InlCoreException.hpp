39a58bf6e1c13f6707f0a5155a91af145c84ea40
NoOverflow <julien.prost@epitech.eu>
2021-06-04 15:39:11 +0200

    Add InlCoreException class

---
+++ inl/include/Core/exceptions/InlCoreException.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2021
+** inl
+** File description:
+** InlCoreException
+*/
+
+#ifndef INLCOREEXCEPTION_HPP_
+#define INLCOREEXCEPTION_HPP_
+
+#include <exception>
+#include <string>
+
+namespace inl {
+namespace core {
+
+    class InlCoreException : public std::exception {
+    public:
+        explicit InlCoreException()
+            : InlCoreException("Generic exception")
+        {
+        }
+
+        explicit InlCoreException(const std::string& message)
+            : msg_(message)
+        {
+        }
+
+        virtual ~InlCoreException() noexcept { }
+
+        virtual const char* what() const noexcept
+        {
+            return (msg_.c_str());
+        }
+
+    protected:
+        std::string msg_;
+
+    private:
+    };
+
+}
+}
+
+#endif /* !INLCOREEXCEPTION_HPP_ */
