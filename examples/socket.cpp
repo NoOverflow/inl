099c378a17eccae416f34e9ca7ba78032f6e807c
NoOverflow <julien.prost@epitech.eu>
2021-06-07 18:25:59 +0200

    Format socket using clangformat

--- examples/socket.cpp
+++ examples/socket.cpp
@@ -2,19 +2,18 @@
 
 #include "inl.hpp"
 
-using inl::core::SocketType;
 using inl::core::Socket;
+using inl::core::SocketType;
 
 int main()
 {
-    try
-    {
+    try {
+        // This example is a bit useless cause you should not need to create a
+        // raw socket at any point
         Socket sock(SocketType::TCP);
 
         std::cout << "Socket created successfully !" << std::endl;
-    }
-    catch(const std::exception& e)
-    {
+    } catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
     return (0);
