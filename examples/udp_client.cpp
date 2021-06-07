9574fa0cd8a18dd9c668e0ce0d9b70e0d141e60f
NoOverflow <julien.prost@epitech.eu>
2021-06-08 00:43:25 +0200

    Add UdpClient example

---
+++ examples/udp_client.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+#include "inl.hpp"
+
+using inl::core::UdpClient;
+
+int main()
+{
+    UdpClient client;
+    std::string line;
+
+    std::cout << "[UDP Client] I will send what you type here to the server."
+              << std::endl;
+    client.set_destination("127.0.0.1", 8080);
+    while (!std::cin.eof()) {
+        std::vector<char> v;
+
+        std::getline(std::cin, line);
+        std::copy(line.begin(), line.end(), std::back_inserter(v));
+        client.send(v);
+    }
+    return 0;
+}
