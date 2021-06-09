2d338c9f0efe8f3719ecb48fb2b94163c439d717
NoOverflow <julien.prost@epitech.eu>
2021-06-09 02:21:33 +0200

    Add TcpClient

---
+++ inl/include/Core/Tcp/TcpClient.hpp
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2021
+** inl
+** File description:
+** TcpClient
+*/
+
+#ifndef TCPCLIENT_HPP_
+#define TCPCLIENT_HPP_
+
+#include <optional>
+#include <string>
+#include <vector>
+
+#ifdef WIN32
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#elif __linux__
+typedef int SOCKET; // Windows uses a typedef called SOCKET as well
+#define INVALID_SOCKET -1
+#define closesocket(s) close(s)
+typedef struct sockaddr_in SOCKADDR_IN;
+typedef struct sockaddr SOCKADDR;
+typedef struct in_addr IN_ADDR;
+#include <arpa/inet.h>
+#include <netdb.h> /* gethostbyname */
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h> /* close */
+#else
+#error INL is not supported on this platform
+#endif
+
+#include "Core/Socket.hpp"
+
+namespace inl {
+namespace core {
+
+    class TcpClient {
+    public:
+        TcpClient();
+        TcpClient(const TcpClient&) = delete;
+        TcpClient(TcpClient&&);
+        TcpClient(
+            SOCKET socket, std::optional<SOCKADDR_IN> m_to = std::nullopt);
+        ~TcpClient();
+
+        void connect(const std::string& ip, unsigned short port);
+        void send(const std::vector<char>& data);
+        std::vector<char> recv(size_t len);
+
+        TcpClient& operator=(TcpClient&&);
+
+    protected:
+    private:
+        Socket m_internal_socket;
+        std::optional<SOCKADDR_IN> m_to = std::nullopt;
+    };
+
+}
+}
+
+#endif /* !TCPCLIENT_HPP_ */
---
+++ inl/src/Core/Tcp/TcpClient.cpp
@@ -0,0 +1,104 @@
+/*
+** EPITECH PROJECT, 2021
+** inl
+** File description:
+** TcpClient
+*/
+
+#include <string>
+
+#include "Core/Tcp/TcpClient.hpp"
+#include "Core/exceptions/InlCoreException.hpp"
+
+namespace inl {
+namespace core {
+
+    TcpClient::TcpClient()
+        : m_internal_socket(TCP)
+    {
+    }
+
+    TcpClient::TcpClient(SOCKET socket, std::optional<SOCKADDR_IN> remote)
+        : m_internal_socket(socket)
+        , m_to(remote)
+    {
+    }
+
+    TcpClient::TcpClient(TcpClient&& other)
+        : m_internal_socket(std::move(other.m_internal_socket))
+    {
+        this->m_to = other.m_to;
+    }
+
+    TcpClient::~TcpClient()
+    {
+    }
+
+    TcpClient& TcpClient::operator=(TcpClient&& other)
+    {
+        this->m_internal_socket = std::move(other.m_internal_socket);
+        this->m_to = other.m_to;
+        return *this;
+    }
+
+    void TcpClient::connect(const std::string& ip, unsigned short port)
+    {
+        struct hostent* hostinfo = NULL;
+        SOCKADDR_IN sin = { 0 };
+
+        if (m_to) {
+            throw InlCoreException("Already connected.");
+        }
+        hostinfo = gethostbyname(ip.c_str());
+        if (!hostinfo) {
+            throw InlCoreException("Couldn't resolve given hostname");
+        }
+        m_to = SOCKADDR_IN {};
+        m_to->sin_addr = *(IN_ADDR*)hostinfo->h_addr;
+        m_to->sin_port = htons(port);
+        m_to->sin_family = AF_INET;
+        if (::connect(
+                m_internal_socket.get_internal_socket(),
+                (SOCKADDR*)&(m_to.value()), sizeof(SOCKADDR))
+            == SOCKET_ERROR) {
+            m_to = std::nullopt;
+            throw InlCoreException("Couldn't connect to given hostname");
+        }
+    }
+
+    void TcpClient::send(const std::vector<char>& data)
+    {
+        size_t total_wrote = 0;
+        int n = 0;
+
+        while (total_wrote != data.size()) {
+            n = ::send(
+                this->m_internal_socket.get_internal_socket(),
+                data.data() + total_wrote, data.size() - total_wrote, 0);
+            if (n < 0) {
+                throw InlCoreException("Couldn't send data");
+            }
+            total_wrote += n;
+        }
+    }
+
+    std::vector<char> TcpClient::recv(size_t len)
+    {
+        std::vector<char> ret(len);
+        size_t total_read = 0;
+        int n = 0;
+
+        while (total_read != len) {
+            n = ::recv(
+                this->m_internal_socket.get_internal_socket(),
+                ret.data() + total_read, len - total_read, 0);
+            if (n < 0) {
+                auto error = WSAGetLastError();
+                throw InlCoreException("Couldn't receive data");
+            }
+            total_read += n;
+        }
+        return ret;
+    }
+}
+}
